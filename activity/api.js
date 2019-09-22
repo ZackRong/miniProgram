5d30c7081388b1b47f86767317fe57a6dc055e0a
rongzhx123 <921586147@qq.com>
2019-09-22 11:34:05 +0800

    文档修改

--- activity/api.js
+++ activity/api.js
@@ -2,12 +2,13 @@ const API = {
   // 登录
   'logon': '/user/logon',
   // 活动列表
-  'listNearbyActivitiesByScene': '/ui/activity/listNearbyActivitiesByScene'
+  'listNearbyActivitiesByScene': '/ui/activity/listNearbyActivitiesByScene',
 };
 
 // module.exports = {
 //   API
 // };
+// 导出API
 export {
   API
 };
